81606cc0b0c0fe4944f2ca39622ddae94dd3580d
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-08-24 13:51:20 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -8,21 +8,21 @@ public class LinkedList {
        linkedlist.add("Sharif");
        linkedlist.add("Srinu");
        linkedlist.add("Anji");
-       System.out.println ("Linked List Content: " +linkedlist);
+        System.out.println ("Linked List Content: " +linkedlist);
 
        linkedlist.addFirst("First Name");
        linkedlist.addLast("Last Name");
-       System.out.println ("LinkedList Content after addition: " +linkedlist);
+        System.out.println ("LinkedList Content after addition: " +linkedlist);
        Object firstvar = linkedlist.get(0);
-       System.out.println("First Name: " +firstvar);
+        System.out.println("First Name: " +firstvar);
        linkedlist.set(0, "Changed first Name");
        Object firstvar2 = linkedlist.get(0);
-       System.out.println ("First Name after update by set method: " +firstvar2);
+        System.out.println ("First Name after update by set method: " +firstvar2);
        linkedlist.removeFirst();
        linkedlist.removeLast();
-       System.out.println("LinkedList after deletion of first and last Name: " +linkedlist);
+        System.out.println("LinkedList after deletion of first and last Name: " +linkedlist);
        linkedlist.add(0, "Dinesh");
        linkedlist.remove(2);
-       System.out.println ("Final Content: " +linkedlist); 
+        System.out.println ("Final Content: " +linkedlist); 
      }
 }
