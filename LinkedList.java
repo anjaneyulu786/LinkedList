3e7d15196f9a2501b379eeccf04e53b26c171c72
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-09-10 20:43:28 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class LinkedList {
      public static void main(String args[]) {
        LinkedList<String> linkedlist = new LinkedList<String>();
-       linkedlist.add("Syam");
-       linkedlist.add("Malik");
-       linkedlist.add("Sharif");
-       linkedlist.add("Srinu");
-       linkedlist.add("Anji");
+       linkedlist.add ("Syam");
+       linkedlist.add ("Malik");
+       linkedlist.add ("Sharif");
+       linkedlist.add ("Srinu");
+       linkedlist.add ("Anji");
         System.out.println ("Linked List Content: " +linkedlist);
 
-       linkedlist.addFirst("First Name");
-       linkedlist.addLast("Last Name");
+       linkedlist.addFirst ("First Name");
+       linkedlist.addLast ("Last Name");
         System.out.println ("LinkedList Content after addition: " +linkedlist);
        Object firstvar = linkedlist.get(0);
         System.out.println("First Name: " +firstvar);
@@ -20,8 +20,8 @@ public class LinkedList {
         System.out.println ("First Name after update by set method: " +firstvar2);
        linkedlist.removeFirst();
        linkedlist.removeLast();
-        System.out.println("LinkedList after deletion of first and last Name: " +linkedlist);
-       linkedlist.add(0, "Dinesh");
+        System.out.println ("LinkedList after deletion of first and last Name: " +linkedlist);
+       linkedlist.add (0, "Dinesh");
        linkedlist.remove(2);
         System.out.println ("Final Content: " +linkedlist); 
      }
